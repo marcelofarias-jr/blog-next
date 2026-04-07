09fcded026dc2a369fbd63ccebc4944d06df9469
José Marcelo <marcelobfariasjr@gmail.com>
2026-04-07 10:12:17 -0300

    refatoração: adição da função ensurePostsTable para verificar e migrar a tabela de posts

--- src/db/drizzle/index.ts
+++ src/db/drizzle/index.ts
@@ -1,4 +1,5 @@
 import { drizzle } from 'drizzle-orm/better-sqlite3';
+import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
 import { postsTable } from './schemas';
 import Database from 'better-sqlite3';
 import { resolve } from 'path';
@@ -12,3 +13,27 @@ export const drizzleDb = drizzle(sqliteDatabase, {
   },
   logger: false,
 });
+
+function ensurePostsTable() {
+  try {
+    sqliteDatabase.prepare('SELECT 1 FROM posts LIMIT 1').get();
+  } catch (error) {
+    if (error instanceof Error) {
+      if (error.message.includes('no such table: posts')) {
+        const migrationsFolder = resolve(
+          process.cwd(),
+          'src',
+          'db',
+          'drizzle',
+          'migrations',
+        );
+        migrate(drizzleDb, { migrationsFolder });
+        return;
+      }
+    }
+
+    throw error;
+  }
+}
+
+ensurePostsTable();
